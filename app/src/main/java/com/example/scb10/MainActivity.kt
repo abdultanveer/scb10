c423723f4d452a2fdec38079b908c303a44fca4a
abdul <abdul.tanveer@gmail.com>
2024-11-27 09:24:24 +0530

    handleClick

--- app/src/main/java/com/example/scb10/MainActivity.kt
+++ app/src/main/java/com/example/scb10/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.scb10
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+    }
+
+    fun handleClick(view: View) {
+        var view:ConstraintLayout = findViewById(R.id.constraintlayout)
+       var snackbar =  Snackbar.make(view,"1 archived",Snackbar.LENGTH_LONG)
+        snackbar.setAction("undo",{})
+        snackbar.show()
     }
 }
