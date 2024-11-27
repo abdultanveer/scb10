3bb98a9af7d970eff0eb56f28f4ba33e5ff6ea91
abdul <abdul.tanveer@gmail.com>
2024-11-27 10:04:48 +0530

    viewbinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.scb10"
     compileSdk = 35
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.scb10"
         minSdk = 24
--- app/src/main/java/com/example/scb10/MainActivity.kt
+++ app/src/main/java/com/example/scb10/MainActivity.kt
@@ -9,19 +9,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.scb10.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var logButton:Button
+   // lateinit var logButton:Button
+   private lateinit var binding: ActivityMainBinding
+var count = 0
     var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        logButton = findViewById(R.id.btnLog)
 
-        logButton.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+       // setContentView(R.layout.activity_main) //inflating xml == parsing and allocating mem
+      //  logButton = findViewById(R.id.btnLog)
+
+        binding.btnLog.setOnClickListener {
             Log.i(TAG,"button was clicked")
+            var name = binding.etName.text.toString()
+            binding.tvMain.setText(name)
+        }
+
+        binding.btnincrease.setOnClickListener {
+            count++
+            binding.tvMain.setText(""+count)
         }
 
     }
