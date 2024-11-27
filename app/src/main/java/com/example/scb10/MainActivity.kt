bb0dbe66ee4a5b8a94084079ac1474cd038f9a65
abdul <abdul.tanveer@gmail.com>
2024-11-27 09:32:10 +0530

    logging onclicklistener

--- app/src/main/java/com/example/scb10/MainActivity.kt
+++ app/src/main/java/com/example/scb10/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.scb10
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -10,15 +12,18 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var logButton:Button
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+        logButton = findViewById(R.id.btnLog)
+
+        logButton.setOnClickListener {
+            Log.i(TAG,"button was clicked")
+        }
+
     }
 
     fun handleClick(view: View) {
