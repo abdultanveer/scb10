b39ad8462c79838714a17c2ab9908ba04f17ad62
abdul <abdul.tanveer@gmail.com>
2024-11-29 09:49:53 +0530

    getting json

---
+++ app/src/main/java/com/example/scb10/InternetActivity.kt
@@ -0,0 +1,32 @@
+package com.example.scb10
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.scb10.databinding.ActivityInternetBinding
+import com.example.scb10.databinding.ActivityMainBinding
+import com.example.scb10.networking.MarsApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class InternetActivity : AppCompatActivity() {
+    lateinit var binding: ActivityInternetBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInternetBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnJson.setOnClickListener { getMarsPhotos() }
+    }
+
+    private fun getMarsPhotos() {
+       GlobalScope.launch(Dispatchers.Main) {
+           val listResult = MarsApi.retrofitService.getPhotos()
+           binding.tvJSon.setText(listResult.toString())
+
+       }
+    }
+}
