9abea9b96ffaaf8d5572ac4e03fcf746f298dec9
abdul <abdul.tanveer@gmail.com>
2024-11-29 11:51:36 +0530

    put parsed json innto recyclerview

--- app/src/main/java/com/example/scb10/InternetActivity.kt
+++ app/src/main/java/com/example/scb10/InternetActivity.kt
@@ -5,29 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import com.example.scb10.databinding.ActivityInternetBinding
 import com.example.scb10.databinding.ActivityMainBinding
 import com.example.scb10.networking.MarsApi
+import com.example.scb10.networking.MarsPhoto
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class InternetActivity : AppCompatActivity() {
     lateinit var binding: ActivityInternetBinding
+    lateinit var adapter:WordListAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInternetBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         binding.btnJson.setOnClickListener { getMarsPhotos() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getMarsPhotos()
+        adapter = WordListAdapter()
+        binding.recyclerView.layoutManager =  LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 
     private fun getMarsPhotos() {
        GlobalScope.launch(Dispatchers.Main) {
-           val url = MarsApi.retrofitService.getPhotos().get(0).imgSrc
-          // binding.tvJSon.setText(listResult.toString())
-           binding.imageView2.load(url)
+           val listMarsphots = MarsApi.retrofitService.getPhotos()
+           adapter.submitList(listMarsphots)
 
        }
     }
---
+++ app/src/main/java/com/example/scb10/WordListAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.scb10
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.scb10.networking.MarsPhoto
+
+class WordListAdapter : ListAdapter<MarsPhoto, WordListAdapter.WordViewHolder>(WordsComparator()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+    }
+    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val wordItemView: TextView = itemView.findViewById(R.id.textViewurl)
+        private val tvid: TextView = itemView.findViewById(R.id.textViewId)
+
+        fun bind(marsPhoto: MarsPhoto) {
+            wordItemView.text = marsPhoto.imgSrc
+            tvid.text =marsPhoto.id
+        }
+        companion object {
+            fun create(parent: ViewGroup): WordViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.visiting_card, parent, false)
+                return WordViewHolder(view)
+            }
+        }
+    }
+    class WordsComparator : DiffUtil.ItemCallback<MarsPhoto>() {
+        override fun areItemsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem === newItem
+        }
+        override fun areContentsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem.imgSrc == newItem.imgSrc
+        }
+    }
+}
